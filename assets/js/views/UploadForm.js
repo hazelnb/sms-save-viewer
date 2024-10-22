926bf5a0de5a3b627ec1eae4e6246db928ca056e
Hazel Brenner <hnb29@cornell.edu>
2024-10-22 10:42:16 -0400

    Landing page improvements

--- assets/js/views/UploadForm.js
+++ assets/js/views/UploadForm.js
@@ -1,5 +1,6 @@
 import m from "https://esm.sh/mithril";
 import Parser from "../models/SaveParser.js";
+import SMSTextBox from "./SMSTextBox.js";
 
 const UploadForm = {
     oncreate: (vnode) => {
@@ -7,10 +8,10 @@ const UploadForm = {
     },
   
     view: () => {
-      return m("form",
+      return m(SMSTextBox, {className: "uploadBox"}, [m("p.contents", "Mario Sunshine Save Viewer"), m("form#uploadForm",
           [m("input#saveFile", {type: "file", accept: ".gci", style: "display:none;", onchange: "Parser.readSave.bind(this)"}),
           m("label#saveFileLabel", {for: "saveFile"}, "Upload your SMS save")]
-      )
+      )])
     }
 }
 
