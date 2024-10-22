2fd2c3ddd4acb09bbf60cbdb61a1893e5cd673c5
Hazel Brenner <hnb29@cornell.edu>
2024-10-22 10:42:34 -0400

    All eclipse areas + blues

--- assets/data/eclipse.js
+++ assets/data/eclipse.js
@@ -1,32 +1,62 @@
 export default {
 	"areas": [
-		{
-			"id": 9,
-			"name": "Erto Rock"
-		},
-		{
-			"id": 10,
-			"name": "Lighthouse Island"
-		},
-		{
-			"id": 11,
-			"name": "Warship Island"
-		},
-		{
-			"id": 12,
-			"name": "Hotel Lacrima"
-		},
+		{ "id": 9, "name": "Erto Rock" },
+		{ "id": 10, "name": "Lighthouse Island" },
+		{ "id": 11, "name": "Warship Island" },
+		{ "id": 12, "name": "Hotel Lacrima" },
 		{
 			"id": 13,
 			"name": "Pianta Pit",
-			"n_shines": 7
+			"n_shines": 7,
+			"shine_names": [
+				"Top of the great tree",
+				"Tramplin Stu Monster on the Loose",
+				"Tree Top Village Chuckster",
+				"Cheep-Cheep Chow Down",
+				"Douse the Campfires",
+				"Mega Mushroom Ground Pound",
+				"100 Coins"
+			]
 		},
 		{
 			"id": 14,
 			"name": "Daisy Cruiser",
 			"n_shines": 6,
-			"n_blues": 10
+			"n_blues": 10,
+			"shine_names": [
+				"Job for a Plumber",
+				"Dine N' Dash",
+				"Durian Excursion",
+				"Flight Path Peril",
+				"Flight Path Peril Red Coins"
+			]
+
+		},
+		{ "id": 15, "name": "Mario's Dream", "n_shines": 5, "n_blues": 10 },
+		{ "id": 16, "name": "Lancia Fredda Peaks", "n_shines": 5, "n_blues": 10 },
+		{ "id": 17, "name": "Vaporwave", "n_shines": 5, "n_blues": 10 },
+		{ "id": 18, "name": "Yoshi Village", "n_shines": 5, "n_blues": 10 },
+		{ "id": 19, "name": "Red Lily City", "n_shines": 5, "n_blues": 10 },
+		{ "id": 20, "name": "Peach Beach", "n_shines": 5, "n_blues": 10 },
+		{ "id": 21, "name": "Spettro Casino", "n_shines": 5, "n_blues": 10 },
+		{ 
+			"id": 22, 
+			"name": "Isle Delfino", 
+			"n_shines": 9, 
+			"n_blues": 0, 
+			"shine_names": [
+				"Bollente Crypt",
+				"Arido Trail",
+				"Arcobaleno Cove",
+				"Sonnolento Grove",
+				"Rugivosa Marsh",
+				"Giovane Creek",
+				"Marea Reef",
+				"Ricco Station",
+				"Delfino Express"
+			  ]
 		}
+
 	],
 	"blues_metadata": {
 		"9": [
@@ -245,17 +275,17 @@ export default {
 				"subway_circuit_num": 19
 			},
 			{
-				"description": "middle pillar small ground poind stone",
+				"description": "middle island- small ground pound stone",
 				"mem_num": 3,
 				"subway_circuit_num": 16
 			},
 			{
-				"description": "floating on wall of largest pillar",
+				"description": "floating on wall of largest island",
 				"mem_num": 4,
 				"subway_circuit_num": 15
 			},
 			{
-				"description": "under smallest pillar ramp",
+				"description": "under smallest island ramp",
 				"mem_num": 5,
 				"subway_circuit_num": 5
 			},
@@ -352,12 +382,12 @@ export default {
 				"subway_circuit_num": 7
 			},
 			{
-				"description": "under back of hotel chicken wire",
+				"description": "chicken wire behind hotel",
 				"mem_num": 4,
 				"subway_circuit_num": 8
 			},
 			{
-				"description": "corner outside hotel entrance",
+				"description": "hotel entrance corner",
 				"mem_num": 5,
 				"subway_circuit_num": 6
 			},
@@ -590,6 +620,371 @@ export default {
 				"mem_num": 10,
 				"subway_circuit_num": 7
 			}
-		]
+		],
+		"15": [
+			{
+				"description": "Low X",
+				"mem_num": 1,
+				"subway_circuit_num": 5
+			},
+			{
+				"description": "High X",
+				"mem_num": 2,
+				"subway_circuit_num": 6
+			},
+			{
+				"description": "Broken pillar M below rainbow bridge",
+				"mem_num": 3,
+				"subway_circuit_num": 10
+			},
+			{
+				"description": "Low broken pillar M",
+				"mem_num": 4,
+				"subway_circuit_num": 4
+			},
+			{
+				"description": "Parthenon M",
+				"mem_num": 5,
+				"subway_circuit_num": 9
+			},
+			{
+				"description": "Broken pillar left of spawn",
+				"mem_num": 6,
+				"subway_circuit_num": 1
+			},
+			{
+				"description": "Low tree platform",
+				"mem_num": 7,
+				"subway_circuit_num": 7
+			},
+			{
+				"description": "Broken pillar on big rotating square path",
+				"mem_num": 8,
+				"subway_circuit_num": 2
+			},
+			{
+				"description": "Star pillar bridge",
+				"mem_num": 9,
+				"subway_circuit_num": 3
+			},
+			{
+				"description": "Brandenburg gate",
+				"mem_num": 10,
+				"subway_circuit_num": 8
+			}
+		],
+		"16": [
+			{
+				"description": "Spawn ice alcove M",
+				"mem_num": 1,
+				"subway_circuit_num": 6
+			},
+			{
+				"description": "Low X",
+				"mem_num": 2,
+				"subway_circuit_num": 8
+			},
+			{
+				"description": "High X",
+				"mem_num": 3,
+				"subway_circuit_num": 7
+			},
+			{
+				"description": "Turret high alcove",
+				"mem_num": 4,
+				"subway_circuit_num": 9
+			},
+			{
+				"description": "Atop mountain ice chip",
+				"mem_num": 5,
+				"subway_circuit_num": 2
+			},
+			{
+				"description": "Atop mountain ice spike",
+				"mem_num": 6,
+				"subway_circuit_num": 4
+			},
+			{
+				"description": "Atop mountain ice ring",
+				"mem_num": 7,
+				"subway_circuit_num": 3
+			},
+			{
+				"description": "High wall hole beneath ramp",
+				"mem_num": 8,
+				"subway_circuit_num": 10
+			},
+			{
+				"description": "Big cube crack",
+				"mem_num": 9,
+				"subway_circuit_num": 5
+			},
+			{
+				"description": "Left of spawn M",
+				"mem_num": 10,
+				"subway_circuit_num": 1
+			}
+		],
+		"17": [
+			{
+				"description": "Behind blue cataquack corral",
+				"mem_num": 1,
+				"subway_circuit_num": 9
+			},
+			{
+				"description": "Two red cataquacks M",
+				"mem_num": 2,
+				"subway_circuit_num": 2
+			},
+			{
+				"description": "Red palm tree near spawn",
+				"mem_num": 3,
+				"subway_circuit_num": 1
+			},
+			{
+				"description": "Blue cataquack near M",
+				"mem_num": 4,
+				"subway_circuit_num": 8
+			},
+			{
+				"description": "Big yellow circle M",
+				"mem_num": 5,
+				"subway_circuit_num": 7
+			},
+			{
+				"description": "White marble M",
+				"mem_num": 6,
+				"subway_circuit_num": 3
+			},
+			{
+				"description": "Atop pagoda",
+				"mem_num": 7,
+				"subway_circuit_num": 4
+			},
+			{
+				"description": "Wall jump over void",
+				"mem_num": 8,
+				"subway_circuit_num": 6
+			},
+			{
+				"description": "Aqueduct corner cut",
+				"mem_num": 9,
+				"subway_circuit_num": 5
+			},
+			{
+				"description": "Tall pink frame",
+				"mem_num": 10,
+				"subway_circuit_num": 10
+			}
+		],
+		"18": [
+			{
+				"description": "Hut floor M",
+				"mem_num": 1,
+				"subway_circuit_num": 9
+			},
+			{
+				"description": "Beach crate",
+				"mem_num": 2,
+				"subway_circuit_num": 3
+			},
+			{
+				"description": "Under start pier",
+				"mem_num": 3,
+				"subway_circuit_num": 5
+			},
+			{
+				"description": "M over void",
+				"mem_num": 4,
+				"subway_circuit_num": 10
+			},
+			{
+				"description": "Atop yoshi flag",
+				"mem_num": 5,
+				"subway_circuit_num": 8
+			},
+			{
+				"description": "Umbrella bounce",
+				"mem_num": 6,
+				"subway_circuit_num": 4
+			},
+			{
+				"description": "M near volleyball net",
+				"mem_num": 7,
+				"subway_circuit_num": 1
+			},
+			{
+				"description": "Above volleyball net",
+				"mem_num": 8,
+				"subway_circuit_num": 6
+			},
+			{
+				"description": "High cliff",
+				"mem_num": 9,
+				"subway_circuit_num": 7
+			},
+			{
+				"description": "Save yellow yoshi",
+				"mem_num": 10,
+				"subway_circuit_num": 2
+			}
+		],
+		"19": [
+			{
+				"description": "Secret room grate",
+				"mem_num": 1,
+				"subway_circuit_num": 3
+			},
+			{
+				"description": "Behind cafe",
+				"mem_num": 2,
+				"subway_circuit_num": 9
+			},
+			{
+				"description": "Small M by graffiti",
+				"mem_num": 3,
+				"subway_circuit_num": 10
+			},
+			{
+				"description": "Ceiling top M",
+				"mem_num": 4,
+				"subway_circuit_num": 1
+			},
+			{
+				"description": "Ramp side triangle",
+				"mem_num": 5,
+				"subway_circuit_num": 6
+			},
+			{
+				"description": "Flat side triangle",
+				"mem_num": 6,
+				"subway_circuit_num": 5
+			},
+			{
+				"description": "Corner crates",
+				"mem_num": 7,
+				"subway_circuit_num": 2
+			},
+			{
+				"description": "Bubble",
+				"mem_num": 8,
+				"subway_circuit_num": 4
+			},
+			{
+				"description": "Blue bird",
+				"mem_num": 9,
+				"subway_circuit_num": 7
+			},
+			{
+				"description": "Playground blue butterflies",
+				"mem_num": 10,
+				"subway_circuit_num": 8
+			}
+		],
+		"20": [
+			{
+				"description": "crate near cruiser",
+				"mem_num": 1,
+				"subway_circuit_num": 1
+			},
+			{
+				"description": "blue butterflies",
+				"mem_num": 2,
+				"subway_circuit_num": 9
+			},
+			{
+				"description": "town X",
+				"mem_num": 3,
+				"subway_circuit_num": 6
+			},
+			{
+				"description": "X by the water",
+				"mem_num": 4,
+				"subway_circuit_num": 5
+			},
+			{
+				"description": "pink arch triangle",
+				"mem_num": 5,
+				"subway_circuit_num": 3
+			},
+			{
+				"description": "above pianta statue",
+				"mem_num": 6,
+				"subway_circuit_num": 7
+			},
+			{
+				"description": "pink arch M",
+				"mem_num": 7,
+				"subway_circuit_num": 4
+			},
+			{
+				"description": "triangle by cruiser",
+				"mem_num": 8,
+				"subway_circuit_num": 2
+			},
+			{
+				"description": "spray the kart",
+				"mem_num": 9,
+				"subway_circuit_num": 8
+			},
+			{
+				"description": "island shine symbol",
+				"mem_num": 10,
+				"subway_circuit_num": 10
+			}
+		],
+		"21": [
+			{
+				"description": "Two of spades red M",
+				"mem_num": 1,
+				"subway_circuit_num": 4
+			},
+			{
+				"description": "Jenga M",
+				"mem_num": 2,
+				"subway_circuit_num": 8
+			},
+			{
+				"description": "Glass jar",
+				"mem_num": 3,
+				"subway_circuit_num": 1
+			},
+			{
+				"description": "Eat boo under tallest card castle",
+				"mem_num": 4,
+				"subway_circuit_num": 6
+			},
+			{
+				"description": "Eat boo near start",
+				"mem_num": 5,
+				"subway_circuit_num": 10
+			},
+			{
+				"description": "Eat boo by small chip stack",
+				"mem_num": 6,
+				"subway_circuit_num": 5
+			},
+			{
+				"description": "Chip stack near dice cup",
+				"mem_num": 7,
+				"subway_circuit_num": 9
+			},
+			{
+				"description": "Green chip stack",
+				"mem_num": 8,
+				"subway_circuit_num": 2
+			},
+			{
+				"description": "Tallest card castle",
+				"mem_num": 9,
+				"subway_circuit_num": 7
+			},
+			{
+				"description": "Black chip stack",
+				"mem_num": 10,
+				"subway_circuit_num": 3
+			}
+		],
+		"22": [],
 	}
 }
